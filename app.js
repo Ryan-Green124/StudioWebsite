c9723415ffe13dd781863aea4987fe843c9f2796
Ryan-Green124 <rjg224@lehigh.edu>
2024-03-08 15:09:06 -0500

    placeholder site

--- app.js
+++ app.js
@@ -5,11 +5,28 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(
-  path.resolve(__dirname, "public")
-));
+// Set the path to the "public" directory
+const publicPath = path.resolve(__dirname, "public");
 
-// Insert code here
+app.use(express.static(publicPath));
 
-app.listen(3000);
+// Define routes
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(publicPath, "index.html"));
+});
 
+app.get("/games", (req, res) => {
+  res.sendFile(path.resolve(publicPath, "gamepage/games.html"));
+});
+
+app.get("/aboutus", (req, res) => {
+  res.sendFile(path.resolve(publicPath, "aboutus/aboutus.html"));
+});
+
+// Insert additional code for other routes as needed
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
